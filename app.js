6bb6e2609a163e44ec67ffa03d6d2f12d5bd35da
Yuriy Tataryntsev <Yuriy_Tataryntsev@epam.com>
2018-02-26 18:36:25 +0200

    done

--- app.js
+++ app.js
@@ -1,67 +1,78 @@
-class CoolComponent extends React.Component {
-    constructor() {
-        super();
+const data = [
+    {
+      title: 'React',
+      src: './img/react.png',
+    },
+    // {
+    //     title: 'Angular',
+    //     src: './img/angular.png',
+    // },
+    // {
+    //     title: 'Ember',
+    //     src: './img/ember.png',
+    // }
+];
+
+class SomeComponent extends React.Component{
+    constructor(props) {
+        super(props);
 
         this.state = {
-            count: 0,
+            countValue: 0,
+            inputValue: 'hello!!',
+            data,
         };
     }
 
+    componentDidMount() {
+        console.log('componentDidMount')
+    }
+
+
     handleClick() {
         this.setState({
-            count: this.state.count + 1,
+            countValue: this.state.countValue + 1,
+        });
+    }
+
+    handleChange(event) {
+        this.setState({
+            inputValue: event.target.value,
         });
     }
 
     render() {
         return (
-            <div onClick={this.handleClick.bind(this)}>
-                <h1>{this.state.count}</h1>
+            <div style={{ background: this.state.inputValue }}>
+                {this.state.inputValue === '000' && 'Hello'}
+                <h1> count value {this.state.countValue}</h1>
                 <h1>{this.props.title}</h1>
-                <img src={this.props.src} />
-                <p>{this.props.description}</p>
+                <img
+                    src={this.props.src}
+                    onClick={this.handleClick.bind(this)}
+                />
+                <input
+                    type='text'
+                    value={this.state.inputValue}
+                    onChange={this.handleChange.bind(this)}
+                />
             </div>
-        );
+        )
     }
 }
 
-const tecknologies = [
-    {
-        id: 1,
-        title: 'React',
-        src: './img/react.png',
-        description: 'Library',
-    },
-    {
-        id: 2,
-        title: 'Angular',
-        src: './img/angular.png',
-        description: 'Framework',
-    },
-    {
-        id: 3,
-        title: 'ember',
-        src: './img/ember.png',
-        description: 'Library',
-    },
-];
-
-function List(props) {
-    return (
-        <div>
-            {props.data.map(tecknologie => (
-                <CoolComponent
-                    key={tecknologie.id}
-                    title={tecknologie.title}
-                    src={tecknologie.src}
-                    description={tecknologie.description}
-                />
-            ))}
-        </div>
-    )
-}
 
 ReactDOM.render(
-    <List data={tecknologies} />,
+    <div>
+        {data.map(elem => {
+            return (
+                <SomeComponent
+                    key={elem.title}
+                    title={elem.title}
+                    src={elem.src}
+                />
+            )
+        })}
+    </div>,
     document.getElementById('root')
 );
